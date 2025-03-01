032f57202af79e09218bc544f075a356e18905a8
Deelolade <Oluwanisholahabeeb@icloud.com>
2025-03-01 11:07:10 +0100

    changed the navlinks text color to 400 again

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,9 +17,9 @@ const Header = () => {
                     <Link to="/" className='text-2xl font-bold italic lowercase '>Dee_Lolade</Link>
                 </div>
                 <ul className="nav-links flex space-x-4 " >
-                    <li><Link to='/' onClick={() => { handleLinkChange("/") }} className={`text-white hover:bg-white hover:text-blue-500 py-2 px-4 rounded-3xl ${selectedLink === "/" ? "bg-white text-blue-400" : ""}`}> Dashboard</Link></li>
-                    <li><Link to='/sign-in' onClick={() => { handleLinkChange("/sign-in") }} className={`text-white hover:bg-white hover:text-blue-500 py-2 px-4 rounded-3xl ${selectedLink === "/sign-in" ? "bg-white  text-blue-400" : ""}`}> Sign In </Link></li>
-                    <li><Link to='/sign-up' onClick={() => { handleLinkChange("/sign-up") }} className={`text-white hover:bg-white hover:text-blue-500 py-2 px-4 rounded-3xl ${selectedLink === "/sign-up" ? "bg-white  text-blue-400" : ""}`}> Sign Up </Link></li>
+                    <li><Link to='/' onClick={() => { handleLinkChange("/") }} className={`text-white hover:bg-white hover:text-blue-400 py-2 px-4 rounded-3xl ${selectedLink === "/" ? " bg-white text-blue-400" : ""}`}> Dashboard</Link></li>
+                    <li><Link to='/sign-in' onClick={() => { handleLinkChange("/sign-in") }} className={`text-white hover:bg-white hover:text-blue-400 py-2 px-4 rounded-3xl ${selectedLink === "/sign-in" ? "bg-white  text-blue-400" : ""}`}> Sign In </Link></li>
+                    <li><Link to='/sign-up' onClick={() => { handleLinkChange("/sign-up") }} className={`text-white hover:bg-white hover:text-blue-400 py-2 px-4 rounded-3xl ${selectedLink === "/sign-up" ? "bg-white  text-blue-400" : ""}`}> Sign Up </Link></li>
                 </ul>
             </nav>
         </div>
